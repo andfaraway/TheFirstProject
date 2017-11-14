e02debbb0dccb316a9b7d84771222961ee28cf3b
Libin <Libin@MacminideMac-mini.local>
2017-11-14 17:52:21 +0800

    new file:   TheFirstProject.xcodeproj/project.pbxproj 	new file:   TheFirstProject.xcodeproj/project.xcworkspace/contents.xcworkspacedata 	new file:   TheFirstProject.xcodeproj/project.xcworkspace/xcuserdata/macmini.xcuserdatad/UserInterfaceState.xcuserstate 	new file:   TheFirstProject.xcodeproj/xcuserdata/macmini.xcuserdatad/xcschemes/TheFirstProject.xcscheme 	new file:   TheFirstProject.xcodeproj/xcuserdata/macmini.xcuserdatad/xcschemes/xcschememanagement.plist 	new file:   TheFirstProject/AppDelegate.h 	new file:   TheFirstProject/AppDelegate.m 	new file:   TheFirstProject/Assets.xcassets/AppIcon.appiconset/Contents.json

---
+++ TheFirstProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TheFirstProject
+//
+//  Created by Libin on 17/11/14.
+//  Copyright © 2017年 Cnmobi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TheFirstProject/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TheFirstProject
+//
+//  Created by Libin on 17/11/14.
+//  Copyright © 2017年 Cnmobi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
